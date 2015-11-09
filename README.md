91e2765c6ee14d954a454ad095854dff8af2b036
Jason Miller <jason@developit.ca>
2015-11-08 23:23:05 -0500

    Packaging

---
+++ README.md
@@ -0,0 +1,7 @@
+# [preact-compat] Example
+
+This is a demo showing how to use [preact-compat] to work with the [react-toolbox] library with [Preact] instead of React.
+
+
+[preact]: https://github.com/developit/preact
+[preact-compat]: https://github.com/developit/preact-compat
---
+++ webpack.config.babel.js
@@ -0,0 +1,64 @@
+import path from 'path';
+import webpack from 'webpack';
+import autoprefixer from 'autoprefixer';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: './build',
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['', '.jsx', '.scss', '.js', '.json'],
+		alias: {
+			'react': 'preact-compat',
+			'react-dom': 'preact-compat'
+		}
+	},
+	module: {
+		preLoaders: [
+			{
+				exclude: /src\//,
+				loader: 'source-map'
+			}
+		],
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: 'babel'
+			},
+			{
+				test: /\.s?css$/,
+				loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap!toolbox')
+			}
+		]
+	},
+	toolbox: {
+		theme: path.join(__dirname, 'src/toolbox-theme.scss')
+	},
+	postcss: [autoprefixer],
+	plugins: ([
+		new webpack.NoErrorsPlugin(),
+		new ExtractTextPlugin('style.css', { allChunks: true }),
+		new webpack.optimize.DedupePlugin(),
+		new HtmlWebpackPlugin()
+	]).concat(process.env.ENVIRONMENT==='production' ? [] : [
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('production')
+		}),
+		new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.optimize.UglifyJsPlugin({
+			output: { comments: false }
+		})
+	]),
+	stats: { colors: true },
+	devtool: 'source-map',
+	devServer: {
+		port: process.env.PORT || 8080,
+		contentBase: './src',
+		historyApiFallback: true
+	}
+};
