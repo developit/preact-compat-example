45652a662e1bc467b5d05e999f2cf2d2c983a10b
Jason Miller <jasonmiller@synacor.com>
2016-03-03 13:07:18 -0500

    Webpack 2 Tree-Shaking support (little or no gain yet, looking into this)

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,35 +1,54 @@
 import path from 'path';
 import webpack from 'webpack';
 import autoprefixer from 'autoprefixer';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import ExtractTextPlugin from 'extract-text-webpack-plugin-webpack-2';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import fs from 'fs';
 
-module.exports = {
+const ENV = String(process.env.ENVIRONMENT || process.env.NODE_ENV || 'development').toLowerCase();
+const PROD = ENV==='production';
+
+let babelrc = {};
+try { babelrc = JSON.parse(fs.readFileSync('.babelrc')); } catch(e) {}
+
+export default {
 	entry: './src/index.js',
+
 	output: {
 		path: './build',
 		filename: 'bundle.js'
 	},
+
 	resolve: {
 		extensions: ['', '.jsx', '.scss', '.js', '.json'],
 		alias: {
 			'react': 'preact-compat',
 			'react-dom': 'preact-compat',
 			'react-addons-css-transition-group': 'rc-css-transition-group'
-		}
+		},
+		packageMains: ['jsnext:main', ['jsnext','main'], 'main'],
+		modules: ['node_modules']
 	},
+
 	module: {
-		preLoaders: [
-			{
-				exclude: /src\//,
-				loader: 'source-map'
-			}
-		],
+		// preLoaders: [
+		// 	{
+		// 		exclude: /src\//,
+		// 		loader: 'source-map'
+		// 	}
+		// ],
 		loaders: [
 			{
 				test: /\.jsx?$/,
 				exclude: /node_modules/,
-				loader: 'babel'
+				loader: 'babel',
+				query: {
+					...babelrc,
+					presets: [
+						'es2015-native-modules',
+						...(babelrc.presets || []).filter( p => !p.match(/^es2015/i) )
+					]
+				}
 			},
 			{
 				test: /\.s?css$/,
@@ -37,28 +56,45 @@ module.exports = {
 			}
 		]
 	},
+
 	toolbox: {
 		theme: path.join(__dirname, 'src/toolbox-theme.scss')
 	},
+
 	postcss: [autoprefixer],
+
 	plugins: ([
+		new webpack.LoaderOptionsPlugin({
+			minimize: PROD,
+			debug: !PROD
+		}),
 		new webpack.NoErrorsPlugin(),
 		new ExtractTextPlugin('style.css', { allChunks: true }),
+		new webpack.DefinePlugin({
+			'process.env': { NODE_ENV: JSON.stringify(ENV) }
+		}),
 		new webpack.optimize.DedupePlugin(),
 		new HtmlWebpackPlugin({
 			template: 'src/index.html'
 		})
-	]).concat(process.env.ENVIRONMENT==='production' ? [
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify('production')
-		}),
-		new webpack.optimize.OccurenceOrderPlugin(),
+	]).concat(PROD ? [
 		new webpack.optimize.UglifyJsPlugin({
+			mangle: false,
+			compress: {
+				warnings: false,
+				dead_code: true,
+				evaluate: true,
+				pure_getters: true,
+				pure_funcs: ['babelHelpers.classCallCheck']
+			},
 			output: { comments: false }
 		})
 	] : []),
+
 	stats: { colors: true },
-	devtool: 'source-map',
+
+	devtool: PROD ? 'hidden-source-map' : 'inline-source-map',
+
 	devServer: {
 		port: process.env.PORT || 8080,
 		contentBase: './src',
